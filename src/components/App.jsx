82ee485e2aa15b87bf323ef0c86534e2379f68ed
TkNazarii <tknazkooo@gmail.com>
2023-04-22 01:02:25 +0300

    second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// import css from './app.module.scss';
+import Form from './_01-form';
+import Filter from './_02-filter';
+import List from './_03-List';
+
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+	value: '',
+    //  contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+  };
+
+  formSubmit = data => {
+    if (data.length >= 0) {
+      this.setState({
+        contacts: [...data],
+      });
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, data],
+      }));
+    }
+  };
+
+  filter = valueFilter => {
+
+	this.setState(prevState => {
+		return { value:  valueFilter };
+	  });
+    
+  };
+
+  render() {
+	// console.log(this.state.value);
+    return (
+      <div>
+		<h1>Phonebook</h1>
+        <Form alert={this.state.contacts} onSubmit={this.formSubmit} />
+		<h2>Contacts</h2>
+		<Filter filter={this.filter}/>
+        <List filterValue={this.state.value || ''} data={this.state.contacts || []} onSubmit={this.formSubmit} />
+      </div>
+    );
+  }
+}
---
+++ src/components/_02-filter/Filter.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import debounce from 'lodash.debounce';
+import PropTypes from "prop-types";
+
+export class Filter extends Component {
+
+	static propTypes = {
+		filter: PropTypes.func.isRequired,
+	  };
+
+  handleValue = evt => {
+	this.props.filter( evt.target.value)
+  };
+
+  render() {
+	// console.log(this.props);
+    return (
+        <label>
+          Find contacts by name{' '}
+          <input
+            // value={this.state.value}
+            type="text"
+            placeholder="Find by name..."
+            onChange={debounce(this.handleValue, 1000)}
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          />
+        </label>
+
+    );
+  }
+}
+
+export default Filter;
